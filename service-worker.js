9892a1ea64b57cd23337b33263fa5cc53092d40e
Sonu-H-N <sonuhn0011@gmail.com>
2026-03-29 21:30:03 +0530

    Upgraded setvice-worker.js code to look professional

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,36 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("expense-tracker").then(cache => {
-      return cache.addAll([
-        "/",
-        "index.html",
-        "style.css",
-        "script.js"
-      ])
-    })
-  )
-})
+const CACHE_NAME = "expense-tracker-v1";
+const urlsToCache = [
+  "/",
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json"
+];
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request)
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
-  )
-})
+  );
+});
